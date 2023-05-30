469cdd06a1f682338b98e6daf8ebb3402dcd1c50
Daniel Prigoshij <prigoshi@pc15.ibr.cs.tu-bs.de>
2023-05-30 12:59:27 +0200

    Finished first test version.

--- drivers/sen5x/sen5x.c
+++ drivers/sen5x/sen5x.c
@@ -21,8 +21,79 @@
 #include "sen5x.h"
 #include "sen5x_constants.h"
 #include "sen5x_params.h"
+#include "sensirion_i2c_hal.h"
+#include "sen5x_i2c.h"
 
-int sen5x_init(sen5x_t *dev, const sen5x_params_t *params)
+void sen5x_init(sen5x_t *dev, const sen5x_params_t *params)
 {
-    
+    /* check parameters */
+    assert(dev && params);
+
+    dev->params = *params;
+
+    i2c_init(dev->params.i2c_dev);
+    sen5x_reset(dev);
+}
+
+void sen5x_reset(sen5x_t *dev) {
+    assert(dev);
+    i2c_acquire(dev->params.i2c_dev);
+
+    sen5x_device_reset();
+
+    i2c_release(dev->params.i2c_dev);
+}
+
+void sen5x_wake(sen5x_t *dev) {
+    assert(dev);
+    i2c_acquire(dev->params.i2c_dev);
+
+    sen5x_start_measurement();
+
+    i2c_release(dev->params.i2c_dev);
+}
+
+void sen5x_sleep(sen5x_t *dev) {
+    assert(dev);
+    i2c_acquire(dev->params.i2c_dev);
+
+    sen5x_stop_measurement();
+
+    i2c_release(dev->params.i2c_dev);
+}
+
+void sen5x_clean_fan(sen5x_t *dev) {
+    assert(dev);
+    i2c_acquire(dev->params.i2c_dev);
+
+    sen5x_start_fan_cleaning();
+
+    i2c_release(dev->params.i2c_dev);
+}
+
+void sen5x_read_values(sen5x_t *dev ,sen5x_measurement_t *values) {
+    assert(dev && values);
+    i2c_acquire(dev->params.i2c_dev);
+
+    sen5x_read_measured_values(
+        &values->mass_concentration_pm1p0, &values->mass_concentration_pm2p5,
+        &values->mass_concentration_pm4p0, &values->mass_concentration_pm10p0,
+        &values->ambient_humidity, &values->ambient_temperature, &values->voc_index,
+        &values->nox_index);
+
+    i2c_release(dev->params.i2c_dev);
+}
+
+void sen5x_read_pm_values(sen5x_t *dev ,sen5x_measurement_t *values) {
+    assert(dev && values);
+    i2c_acquire(dev->params.i2c_dev);
+
+    sen5x_read_measured_pm_values(
+        &values->mass_concentration_pm1p0, &values->mass_concentration_pm2p5,
+        &values->mass_concentration_pm4p0, &values->mass_concentration_pm10p0,
+        &values->number_concentration_pm0p5, &values->number_concentration_pm1p0,
+        &values->number_concentration_pm2p5, &values->number_concentration_pm4p0,
+        &values->number_concentration_pm10p0, &values->typical_particle_size);
+
+    i2c_release(dev->params.i2c_dev);
 }
