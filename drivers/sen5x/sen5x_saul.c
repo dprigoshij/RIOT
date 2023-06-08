fae6ca517036d45ee62a83c31ffcb3934b225bd2
Daniel Prigoshij <prigoshi@pc15.ibr.cs.tu-bs.de>
2023-06-08 15:49:30 +0200

    SAUL source code finished.

--- drivers/sen5x/sen5x_saul.c
+++ drivers/sen5x/sen5x_saul.c
@@ -29,7 +29,7 @@ static int read_mass_concentration_pm1p0(const void *dev, phydat_t *res) {
     
     res->val[0] = (int16_t)values->mass_concentration_pm1p0;
     res->unit   = UNIT_GPM3;
-    res->scale  = -6;  // TODO. cm^3 zu m^3 ausrechnen
+    res->scale  = -5;
 
     return 1;
 }
@@ -42,7 +42,7 @@ static int read_mass_concentration_pm2p5(const void *dev, phydat_t *res) {
     
     res->val[0] = (int16_t)values->mass_concentration_pm2p5;
     res->unit   = UNIT_GPM3;
-    res->scale  = -6;
+    res->scale  = -5;
 
     return 1;
 }
@@ -55,7 +55,7 @@ static int read_mass_concentration_pm4p0(const void *dev, phydat_t *res) {
     
     res->val[0] = (int16_t)values->mass_concentration_pm4p0;
     res->unit   = UNIT_GPM3;
-    res->scale  = -6;
+    res->scale  = -5;
 
     return 1;
 }
@@ -68,7 +68,7 @@ static int read_mass_concentration_pm10p0(const void *dev, phydat_t *res) {
     
     res->val[0] = (int16_t)values->mass_concentration_pm10p0;
     res->unit   = UNIT_GPM3;
-    res->scale  = -6;
+    res->scale  = -5;
 
     return 1;
 }
@@ -81,7 +81,7 @@ static int read_number_concentration_pm0p5(const void *dev, phydat_t *res) {
 
     res->val[0] = (int16_t)values->number_concentration_pm0p5;
     res->unit   = UNIT_CPM3;
-    res->scale  = 0;
+    res->scale  = -5;
 
     return 1;
 }
@@ -94,7 +94,7 @@ static int read_number_concentration_pm1p0(const void *dev, phydat_t *res) {
 
     res->val[0] = (int16_t)values->number_concentration_pm1p0;
     res->unit   = UNIT_CPM3;
-    res->scale  = 0;
+    res->scale  = -5;
     return 1;
 }
 
@@ -106,7 +106,7 @@ static int read_number_concentration_pm2p5(const void *dev, phydat_t *res) {
 
     res->val[0] = (int16_t)values->number_concentration_pm2p5;
     res->unit   = UNIT_CPM3;
-    res->scale  = 0;
+    res->scale  = -5;
     return 1;
 }
 
@@ -118,7 +118,7 @@ static int read_number_concentration_pm4p0(const void *dev, phydat_t *res) {
 
     res->val[0] = (int16_t)values->number_concentration_pm4p0;
     res->unit   = UNIT_CPM3;
-    res->scale  = 0;
+    res->scale  = -5;
 
     return 1;
 }
@@ -131,7 +131,7 @@ static int read_number_concentration_pm10p0(const void *dev, phydat_t *res) {
 
     res->val[0] = (int16_t)values->number_concentration_pm10p0;
     res->unit   = UNIT_CPM3;
-    res->scale  = 0;
+    res->scale  = -5;
 
     return 1;
 }
@@ -139,9 +139,12 @@ static int read_number_concentration_pm10p0(const void *dev, phydat_t *res) {
 static int read_typical_particle_size(const void *dev, phydat_t *res) {
     const sen5x_t *d = (sen5x_t *)dev;
 
-    res->val[0] = ;
-    res->unit   = ;
-    res->scale  = ;
+    sen5x_measurement_t *values;
+    sen5x_read_pm_values(d, values);
+
+    res->val[0] = (int16_t)values->typical_particle_size;
+    res->unit   = UNIT_M;
+    res->scale  = -3;
 
     return 1;
 }
@@ -149,9 +152,12 @@ static int read_typical_particle_size(const void *dev, phydat_t *res) {
 static int read_ambient_humidity(const void *dev, phydat_t *res) {
     const sen5x_t *d = (sen5x_t *)dev;
 
-    res->val[0] = ;
-    res->unit   = ;
-    res->scale  = ;
+    sen5x_measurement_t *values;
+    sen5x_read_pm_values(d, values);
+
+    res->val[0] = (int16_t)values->ambient_humidity;
+    res->unit   = UNIT_PERCENT;
+    res->scale  = 2;
 
     return 1;
 }
@@ -159,17 +165,85 @@ static int read_ambient_humidity(const void *dev, phydat_t *res) {
 static int read_ambient_temperature(const void *dev, phydat_t *res) {
     const sen5x_t *d = (sen5x_t *)dev;
 
-    res->val[0] = ;
-    res->unit   = ;
-    res->scale  = ;
+    sen5x_measurement_t *values;
+    sen5x_read_pm_values(d, values);
+
+    res->val[0] = ((int16_t)values->ambient_temperature) / 2;
+    res->unit   = UNIT_TEMP_C;
+    res->scale  = 2;
 
     return 1;
 }
 
-/**
-const saul_driver_t vcnl40x0_proximity_saul_driver = {
-    .read = read_proximity,
+
+const saul_driver_t sen5x_mass_concentration_pm1p0_driver = {
+    .read = read_mass_concentration_pm1p0,
     .write = saul_write_notsup,
-    .type = SAUL_SENSE_PROXIMITY
+    .type = SAUL_SENSE_PM
 };
-*/
+
+const saul_driver_t sen5x_mass_concentration_pm2p5_driver = {
+    .read = read_mass_concentration_pm2p5,
+    .write = saul_write_notsup,
+    .type = SAUL_SENSE_PM
+};
+
+const saul_driver_t sen5x_mass_concentration_pm4p0_driver = {
+    .read = read_mass_concentration_pm4p0,
+    .write = saul_write_notsup,
+    .type = SAUL_SENSE_PM
+};
+
+const saul_driver_t sen5x_mass_concentration_pm10p0_driver = {
+    .read = read_mass_concentration_pm10p0,
+    .write = saul_write_notsup,
+    .type = SAUL_SENSE_PM
+};
+
+const saul_driver_t sen5x_number_concentration_pm0p5_driver = {
+    .read = read_number_concentration_pm0p5,
+    .write = saul_write_notsup,
+    .type = SAUL_SENSE_COUNT
+};
+
+const saul_driver_t sen5x_number_concentration_pm1p0_driver = {
+    .read = read_number_concentration_pm1p0,
+    .write = saul_write_notsup,
+    .type = SAUL_SENSE_COUNT
+};
+
+const saul_driver_t sen5x_number_concentration_pm2p5_driver = {
+    .read = read_number_concentration_pm2p5,
+    .write = saul_write_notsup,
+    .type = SAUL_SENSE_COUNT
+};
+
+const saul_driver_t sen5x_number_concentration_pm4p0_driver = {
+    .read = read_number_concentration_pm4p0,
+    .write = saul_write_notsup,
+    .type = SAUL_SENSE_COUNT
+};
+
+const saul_driver_t sen5x_number_concentration_pm10p0_driver = {
+    .read = read_number_concentration_pm10p0,
+    .write = saul_write_notsup,
+    .type = SAUL_SENSE_COUNT
+};
+
+const saul_driver_t sen5x_typical_particle_size_driver = {
+    .read = read_typical_particle_size,
+    .write = saul_write_notsup,
+    .type = SAUL_SENSE_SIZE
+};
+
+const saul_driver_t sen5x_ambient_humidity_driver = {
+    .read = read_typical_particle_size,
+    .write = saul_write_notsup,
+    .type = SAUL_SENSE_HUM
+};
+
+const saul_driver_t sen5x_ambient_temperature_driver = {
+    .read = read_typical_particle_size,
+    .write = saul_write_notsup,
+    .type = SAUL_SENSE_TEMP
+};
