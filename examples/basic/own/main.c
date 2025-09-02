5c2b33a45c62cb75d2fb091191a679ed6541c361
Daniel Prigoshij <d.prigoshij@tu-braunschweig.de>
2025-09-02 16:31:33 +0200

    Working implementation for a uart connection

--- examples/basic/own/main.c
+++ examples/basic/own/main.c
@@ -1,36 +1,41 @@
-/**
- * @ingroup     examples
- * @{
- *
- * @file
- * @brief       
- *
- * @author      
- *
- * @}
- */
 
 #include <stdio.h>
 #include "periph/uart.h"
 #include "xtimer.h"
-
-#define UART_DEV        UART_DEV(0)   
+#include "periph/gpio.h"
+  
 #define BAUDRATE        115200
 
+static char rx_buf[64];
+static size_t rx_pos = 0;
 
-static void rx_cb(void *arg, uint8_t data)
-{
+static void rx_cb(void *arg, uint8_t data) {
     (void)arg;
-    
-    printf("Uart data received: %d", (int)data);
+    if (data == '\n' || rx_pos >= sizeof(rx_buf) - 1) {
+        rx_buf[rx_pos] = '\0';  // terminate string
+        printf("UART data received: %s\n", rx_buf);
+        rx_pos = 0;             // reset buffer
+    }
+    else {
+        rx_buf[rx_pos++] = (char)data;
+    }
 }
 
-int main(void)
-{
-    if (uart_init(UART_DEV, BAUDRATE, rx_cb, NULL) != 0) {
+int main(void) {
+
+    if (uart_init(UART_DEV(2), BAUDRATE, rx_cb, NULL) != 0) {
         puts("UART init failed!");
         return 1;
     }
+
+    int counter = 0;
+    while (1) {
+        char buf[32];
+        int len = snprintf(buf, sizeof(buf), "Hello %d\n", counter++);
+        uart_write(UART_DEV(2), (const uint8_t *)buf, len);
+        xtimer_sleep(1);
+    }
+
 
     return 0;
 }
